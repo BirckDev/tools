4d339bdb07ad24d97b3d8482811518d405f75012
Carsten Birck Jensen <g@bircks.dk>
2023-03-15 01:53:23 +0100

    Autocreate download folder if it does not exist

--- ytdownloader/main.py
+++ ytdownloader/main.py
@@ -1,20 +1,24 @@
 from pytube import YouTube
 from halo import Halo
-from time import sleep
+from pathlib import Path
 
 
-def download(link):
+def setup(download_path):
+    Path(download_path).mkdir(parents=True, exist_ok=True)
+
+def download(link, download_path):
     youtube_object = YouTube(link)
     youtube_object = youtube_object.streams.get_highest_resolution()
     try:
         with Halo(text='Downloading...', spinner='dots'):
-            youtube_object.download('youtubes/')
+            youtube_object.download(download_path)
     except:
         print('Downloading went wrong...')
     print('Download succeded.')
 
+download_directory = 'youtubes/'
 
-
+setup(download_directory)
 link = input('Enter YouTube video url: ')
-download(link)
+download(link, download_directory)
 
